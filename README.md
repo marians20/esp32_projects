ac64546b9356d83a1afab8b9b5decba0a03ca1c2
Marian Spoiala <marian.spioiala@gmail.com>
2023-06-17 09:17:40 +0300

    Added soma initial files

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-"# esp32_projects" 
+# esp32_projects
---
+++ init.cmd
@@ -0,0 +1,7 @@
+echo # esp32_projects >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/marians20/esp32_projects.git
+git push -u origin main
